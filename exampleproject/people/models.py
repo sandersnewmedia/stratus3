10cb7a1b67acaa991679d2a4fb7c73b2b0f48f91
Sean Brant <seanbrant@sandersnewmedia.com>
2013-02-22 13:27:44 -0600

    work in progress

--- exampleproject/people/models.py
+++ exampleproject/people/models.py
@@ -4,6 +4,12 @@ from django.utils.translation import ugettext_lazy as _
 
 class Person(models.Model):
     name = models.CharField(_('name'), max_length=255)
+    hobbies = models.TextField(_('hobbies'))
+    is_active = models.BooleanField(_('active'))
+    age = models.IntegerField(_('age'))
+    likes = models.CommaSeparatedIntegerField(_('likes'), max_length=255)
+    birth_date = models.DateTimeField(_('birth date'))
+    avatar = models.ImageField(_('avatar'), upload_to='avatars')
 
 
 class Team(models.Model):
--- exampleproject/people/stratus_admin.py
+++ exampleproject/people/stratus_admin.py
@@ -1,7 +1,28 @@
+from django.contrib import admin
+
 import stratus
 
+from people.models import Person, Team
 
-class PeopleSection(stratus.StratusAppSection):
+
+class PeopleSection(stratus.StratusSection):
     pass
 
+
+class TeamInline(admin.TabularInline):
+    model = Team
+
+
+class PersonModelAdmin(stratus.StratusModelAdmin):
+    save_on_top = True
+    readonly_fields = ['name']
+    inlines = [TeamInline]
+
+
+class PersonPage(stratus.StratusModelAdminPage):
+    model = Person
+    admin_class = PersonModelAdmin
+
+
 people = stratus.site.register(PeopleSection)
+people.register(PersonPage)
