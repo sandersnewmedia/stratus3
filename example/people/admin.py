096a4bc769311e7e11905a3165361a2888aba03b
Sean Brant <seanbrant@sandersnewmedia.com>
2013-03-01 17:07:19 -0600

    More style work

--- example/people/admin.py
+++ example/people/admin.py
@@ -3,5 +3,5 @@ import stratus
 from people.models import Person, Team
 
 
-stratus.site.register(Person, list_display=['name', 'order'], search_fields=['name'], date_hierarchy='birthdate', list_filter=['name'], list_editable=['order'])
+stratus.site.register(Person, list_display=['name', 'order'], search_fields=['name'], date_hierarchy='birthdate', list_filter=['name'], actions_on_bottom=True)
 stratus.site.register(Team)
--- example/submissions/admin.py
+++ example/submissions/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
 import stratus
-from stratus.filters import DateRangeListFilter, SearchListFilter
+from stratus.filters import DateRangeListFilter
 
 from submissions.models import Submission
 
@@ -11,7 +11,6 @@ class SubmissionAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     list_filter = [
         ('created_date', DateRangeListFilter),
-        ('title', SearchListFilter),
         'status',
     ]
     search_fields = ['title']
