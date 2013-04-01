6f7a5a13809c343558a4e69c0ebf3cbf1498427e
Sean Brant <seanbrant@sandersnewmedia.com>
2013-04-01 14:41:26 -0500

    merge

---
+++ stratus/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+from stratus.models import Block
+
+
+class BlockForm(forms.ModelForm):
+    content_single_line_text = forms.CharField(label=_('Single Line Text'), widget=forms.TextInput(attrs={'class': 'vLargeTextField'}), required=False)
+    content_multiple_line_text = forms.CharField(label=_('Multiple Line Text'), widget=forms.Textarea(attrs={'class': 'vLargeTextField'}), required=False)
+    content_list_text = forms.CharField(label=_('List Text'), widget=forms.Textarea(attrs={'class': 'vLargeTextField'}), required=False)
+    content_html = forms.CharField(label=_('HTML'), widget=forms.Textarea(attrs={'class': 'vLargeTextField'}), required=False)
+
+    class Media:
+        js = ['stratus/js/blockgroup.js']
+
+    class Meta(object):
+        model = Block
+        fields = ['key', 'content_type']
+
+    def __init__(self, *args, **kwargs):
+        super(BlockForm, self).__init__(*args, **kwargs)
+        content_type = self.initial.get('content_type')
+        self.initial['content_%s' % content_type] = self.instance.content
+
+    def save(self, commit):
+        content_type = self.cleaned_data['content_type']
+        self.instance.content = self.cleaned_data['content_%s' % content_type]
+        return super(BlockForm, self).save(commit)
---
+++ stratus/static/stratus/js/imagegallery.js
@@ -0,0 +1,130 @@
+(function(Backbone, _, $) {
+
+    var Image = Backbone.Model.extend({
+        url: function() {
+            var url = Backbone.Model.prototype.url.call(this);
+            return (_.last(url) != '/') ? url + '/' : url;
+        }
+    });
+
+    var ImageList = Backbone.Collection.extend({
+        model: Image,
+
+        initialize: function(options) {
+            this.url = options.baseUrl;
+        },
+
+        bulkUpdate: function() {
+            return Backbone.sync('update', this, {
+                success: _.bind(function(model, resp, xhr) {
+                    this.reset(resp);
+                }, this)
+            });
+        }
+    });
+
+    var ImageView = Backbone.View.extend({
+        attributes: {
+            'class': 'thumbnail span2 inline-related',
+        },
+
+        events: {
+            'click .delete': 'deleteModel',
+        },
+
+        initialize: function(options) {
+            this.template = _.template($('#image-template').html());
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            this.$el.attr('id', 'image-' + this.model.id);
+            return this;
+        },
+
+        deleteModel: function() {
+            this.model.destroy({success: _.bind(function() {;
+                this.trigger('deleteModel');
+            }, this)});
+        }
+    });
+
+    window.ImageInline = Backbone.View.extend({
+        events: {
+            'click .add-row a': 'openSelector',
+        },
+
+        initialize: function() {
+            _.bindAll(this, 'openSelector', 'uploadSuccess', 'updateAll');
+
+            this.images = new ImageList({baseUrl: this.options.baseUrl});
+
+            this.addRowEl = this.$('.add-row');
+            this.imageListEl = this.$('.images-list');
+            this.fileInputEl = this.$el.find('.image-file-input');
+
+            this.listenTo(this.images, 'add', this.addOne);
+            this.listenTo(this.images, 'reset', this.addAll);
+
+            this.fileInputEl.fileupload({
+                url: this.images.url,
+                dataType: 'json',
+                autoUpload: true,
+                success: this.uploadSuccess
+            });
+
+            this.imageListEl.sortable({
+                containment: this.$el,
+                helper: _.bind(function(event, el) {
+                    return el.removeClass('thumbnail').addClass('dragging');
+                }),
+                deactivate: _.bind(function(event, ui) {
+                    $(ui.item).addClass('thumbnail').removeClass('dragging');
+                    this.updateAll();
+                }, this)
+            });
+
+            this.images.fetch();
+        },
+
+        addOne: function(image) {
+            var view = new ImageView({model: image});
+            this.imageListEl.append(view.render().el);
+        },
+
+        addAll: function() {
+            this.imageListEl.html('');
+            this.images.each(this.addOne, this);
+        },
+
+        openSelector: function(event) {
+            this.fileInputEl.click();
+            return false;
+        },
+
+        uploadSuccess: function(data) {
+            this.images.add(data);
+            return false;
+        },
+
+        updateAll: function() {
+            var ids = this.imageListEl.sortable('toArray');
+            if (ids.length) {
+                _.each(ids, this.updateOne, this);
+                this.images.bulkUpdate();
+            }
+        },
+
+        updateOne: function(id, order) {
+            var id = id.replace('image-', '');
+            var model = this.images.get(id);
+
+            if (model) {
+                model.set({order: order});
+            }
+        }
+    });
+
+})(Backbone, _, jQuery);
