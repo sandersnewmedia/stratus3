ffdbdbd482962a9362d640ef7ef0b0452fd03da5
Sean Brant <seanbrant@sandersnewmedia.com>
2013-04-04 12:52:31 -0500

    Make sure to include package data

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='stratus',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(exclude=['example']),
+    include_package_data=True,
     zip_safe=False,
 )
