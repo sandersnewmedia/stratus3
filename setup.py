cd8bfe305f8e49bf9c4099b12978fe431c5de154
Sean Brant <seanbrant@sandersnewmedia.com>
2013-04-11 14:10:31 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stratus',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(exclude=['example']),
     include_package_data=True,
     zip_safe=False,
