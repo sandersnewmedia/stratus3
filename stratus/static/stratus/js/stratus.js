bf4caf7380ddb077b0ebc1bdca2e25f5966a3910
Sean Brant <seanbrant@sandersnewmedia.com>
2013-04-15 10:59:21 -0500

    Makes the delete button nicer

---
+++ stratus/static/stratus/js/stratus.js
@@ -0,0 +1,12 @@
+(function($) {
+
+    $(document).ready(function() {
+        $('.js-delete .btn').on('click', function() {
+            var button = $(this);
+            var checkbox = button.parent().find('input');
+            checkbox.prop('checked', !checkbox.prop('checked'));
+            button.text(checkbox.prop('checked') ? button.data('on-text') :  button.data('off-text'));
+        });
+    });
+
+})(jQuery);
