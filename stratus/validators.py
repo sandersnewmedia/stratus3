e249ed65644435368ab9c5dd641c106d2c69af84
Sean Brant <seanbrant@sandersnewmedia.com>
2013-04-15 12:31:19 -0500

    Removes blocks and galleries from stratus

--- stratus/validators.py
+++
@@ -1,7 +0,0 @@
-import re
-
-from django.core.validators import RegexValidator
-from django.utils.translation import ugettext_lazy as _
-
-key_re = re.compile(r'^[a-zA-Z0-9_]+$')
-validate_key = RegexValidator(key_re, _("Enter a valid 'key' consisting of letters, numbers, underscores."), 'invalid')
