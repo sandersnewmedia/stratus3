3a7bf23038b9e53be137589c9eed6df1ede07276
Sean Brant <seanbrant@sandersnewmedia.com>
2013-04-24 15:25:52 -0500

    Adds Status image widget

--- stratus/widgets.py
+++ stratus/widgets.py
@@ -8,6 +8,9 @@ class StratusFileWidget(forms.ClearableFileInput):
         'data-on-text="Deleting..." data-off-text="Mark Deleted?">Mark Deleted?</button>'
         '<span style="display: none;">%(clear)s</span></span></p>')
 
+    def get_initial(self, value):
+        return '<a class="btn" href="%s">View</a>' % value.url
+
     def render(self, name, value, attrs=None):
         substitutions = {
             'clear_template': '',
@@ -18,7 +21,7 @@ class StratusFileWidget(forms.ClearableFileInput):
 
         if value and hasattr(value, 'url'):
             template = self.template_with_initial
-            substitutions['initial'] = '<a class="btn" href="%s">View</a>' % value.url
+            substitutions['initial'] = self.get_initial(value)
 
             if not self.is_required:
                 checkbox_name = self.clear_checkbox_name(name)
@@ -27,3 +30,26 @@ class StratusFileWidget(forms.ClearableFileInput):
                 substitutions['clear_template'] = self.template_with_clear % substitutions
 
         return mark_safe(template % substitutions)
+
+
+class StratusImageWidget(StratusFileWidget):
+
+    def __init__(self, resize=None, *args, **kwargs):
+        self.resize = resize or self.default_resize
+        super(StratusImageWidget, self).__init__(*args, **kwargs)
+
+    def default_resize(self, image):
+        return image, None, None
+
+    def get_initial(self, value):
+        thumbnail, width, height = self.resize(value)
+        return (
+            '<a href="%(full_url)s">'
+            '<img src="%(thumbnail_url)s" %(width)s %(height)s />'
+            '</a>' % {
+                'full_url': value.url,
+                'thumbnail_url': thumbnail.url,
+                'width': width and 'width="%s"' % width or '',
+                'height': height and 'height="%s"' % height or '',
+            }
+        )
