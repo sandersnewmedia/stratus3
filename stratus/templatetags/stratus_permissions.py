af00bfa9ea8b18a3219262ec64c7d77ab6917550
Sean Brant <seanbrant@sandersnewmedia.com>
2013-06-25 10:21:15 -0500

    Since we are not setting object level editing permissions only pass the permission name

--- stratus/templatetags/stratus_permissions.py
+++ stratus/templatetags/stratus_permissions.py
@@ -7,4 +7,4 @@ register = template.Library()
 @register.assignment_tag
 def can(user, permission, obj):
     perm = '%s.%s_%s' % (obj._meta.app_label, permission, obj._meta.module_name)
-    return user.has_perm(perm, obj)
+    return user.has_perm(perm)
